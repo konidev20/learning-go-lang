0af33b53caa18209d149638fc07620ab54380653
konidev20 <sgovind.dev@outlook.com>
2020-09-06 21:34:46 +0530

    Learnt how to use values

---
+++ values/values.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	//string concatenation
+	fmt.Println("Hello" + "World")
+
+	//Integers and floats
+	fmt.Println("1 + 1 = ", 1+1)
+	fmt.Println("1.0 / 2.0 = ", 1.0/2.0)
+
+	//Boolean
+	fmt.Println(true)
+	fmt.Println(false)
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
